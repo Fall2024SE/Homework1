3320fcfc2f4631a9ed452ce21a3b96b4c5b41ee8
Brijesh Kumar <39380117+KoruptTinker@users.noreply.github.com>
2024-08-23 20:02:44 +0000

    Add: Python tests

---
+++ homework1.py
@@ -0,0 +1,16 @@
+def binary_search(arr, left, right, key):
+    if(left > right):
+        return False;
+    
+    pivot = (left + right) // 2
+
+    if(arr[pivot] == key):
+        return True
+
+    if(arr[pivot] > key):
+        return binary_search(arr, left, pivot-1, key)
+    else:
+        return binary_search(arr, pivot+1, right, key)
+
+if(__name__ == "__main__") :
+    print(binary_search([1,2,3,4,5], 0, 10, 3));
---
+++ test_hw1.py
@@ -0,0 +1,7 @@
+import homework1
+
+def test_pass():
+    assert homework1.binary_search([1,2,3,4,5], 0, 4, 3)
+
+def test_fail():
+    assert homework1.binary_search([1,2,3,4,5], 0, 10, 3)
