9054b90cc5819fa79f6194b6b7a0e188212474ca
Brijesh Kumar <39380117+KoruptTinker@users.noreply.github.com>
2024-08-28 19:21:55 -0400

    Update test_hw1.py

--- test_hw1.py
+++ test_hw1.py
@@ -4,5 +4,5 @@ def test_pass():
     assert homework1.binary_search([1,2,3,4,5], 0, 4, 3)
 
     
-# def test_fail():
-#     assert homework1.binary_search([1,2,3,4,5], 0, 10, 9) == False
+def test_fail():
+    assert homework1.binary_search([1,2,3,4,5], 0, 10, 9) == False
