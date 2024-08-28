6a4877362827545464731221a0aaf41eb48b80cb
Brijesh Kumar <39380117+KoruptTinker@users.noreply.github.com>
2024-08-28 09:18:02 -0400

    Update: test_hw1.py

--- test_hw1.py
+++ test_hw1.py
@@ -4,6 +4,5 @@ def test_pass():
     assert homework1.binary_search([1,2,3,4,5], 0, 4, 3)
 
     
-Commenting for test badge
 def test_fail():
     assert homework1.binary_search([1,2,3,4,5], 0, 10, 3)
